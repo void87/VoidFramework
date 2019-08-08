83161e890d950d2d6bd2b50cd5f809b2465785da
void87 <void87@163.com>
2019-08-08 09:46:19 +0800

    1. 增加QFramework部分功能

---
+++ Assets/QFramework/6.MenuItem复用/ReuseMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System;
+
+namespace QFramework {
+
+    public class ReuseMenuItem {
+
+#if UNITY_EDITOR
+        [MenuItem("QFramework/6.MenuItem复用")]
+        private static void MenuClicked() {
+            EditorApplication.ExecuteMenuItem("QFramework/4.导出UnityPackage");
+            Application.OpenURL("file://" + Path.Combine(Application.dataPath, "../"));
+        }
+#endif
+    }
+
+}
---
+++ Assets/QFramework/7.自定义快捷键 %e/CustomShortCut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using System;
+
+namespace QFramework {
+
+    public class CustomShortCut {
+
+#if UNITY_EDITOR
+        [MenuItem("QFramework/7.自定义快捷键 %e")]
+        private static void MenuClicked() {
+            EditorApplication.ExecuteMenuItem("QFramework/6.MenuItem复用");
+        }
+#endif
+    }
+
+}
